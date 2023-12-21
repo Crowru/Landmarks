e43157f7937753c5b54a4596f8756f424f163de3
Ruslan <ruslanusmanov7779@gmail.com>
2023-12-21 18:34:42 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Руслан  on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
