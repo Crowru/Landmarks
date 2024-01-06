b845c75fa073f4adaae373e6d4a995d59f3179e8
Vladislav <vlatyshev@icerockdev.com>
2024-01-06 22:04:03 +0300

    display a user profile

---
+++ Landmarks/App/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Руслан  on 06.01.2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
